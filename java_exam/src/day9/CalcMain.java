eb43e673ed84e6a95ac3113abac5c188b59c466d
codingjs12 <202245068@itc.ac.kr>
2025-01-08 15:58:12 +0900

    20250108

--- java_exam/src/day9/CalcMain.java
+++ java_exam/src/day9/CalcMain.java
@@ -3,18 +3,16 @@ package day9;
 public class CalcMain {
 
 	public static void main(String[] args) {
-		
+
 //		Calculator calculator = new Calculator(); // 추상클래스는 객체생성 불가능
-		
+
 		GoodCalc calc = new GoodCalc();
-		
+
 		int div = calc.div(5, 10);
 		int mod = calc.mod(5, 2);
 		int sum = calc.sum(3, 2);
 		int mul = calc.mul(3, 5);
 		int sub = calc.sub(10, 5);
-		
-		
 
 	}
 
