f6ddb2b4c9204760603ffdf4cb1ce523ff250a11
codingjs12 <202245068@itc.ac.kr>
2025-01-09 18:18:45 +0900

    250109 박싱, 언박싱

---
+++ java_exam/src/day10/Boxing.java
@@ -0,0 +1,21 @@
+package day10;
+
+public class Boxing {
+
+	public static void main(String[] args) {
+
+		int num = 10;
+
+		Integer ten = 10;
+
+		if (num == ten) {
+			System.out.println("true");
+		}
+		
+		if(ten.equals(num)) {
+			System.out.println("true");
+		}
+
+	}
+
+}
