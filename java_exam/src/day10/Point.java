6c1c24c5fc558584948a4dd9c24add122a61511d
codingjs12 <202245068@itc.ac.kr>
2025-01-09 17:37:17 +0900

    250109 객체 속성

---
+++ java_exam/src/day10/Point.java
@@ -0,0 +1,18 @@
+package day10;
+
+public class Point {
+	
+	private int x;
+	private int y;
+	
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void setPoint(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+}
---
+++ java_exam/src/day10/PointMain.java
@@ -0,0 +1,14 @@
+package day10;
+
+public class PointMain {
+
+	public static void main(String[] args) {
+
+		Point p1 = new Point(1, 2);
+		Point p2 = new Point(1, 2);
+
+		System.out.println(p1 == p2);
+
+	}
+
+}
