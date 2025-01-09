6d35dc056011996c22b9230871afff50c2e89583
codingjs12 <202245068@itc.ac.kr>
2025-01-09 18:07:09 +0900

    250109 equals 오버라이딩

--- java_exam/src/day10/Point.java
+++ java_exam/src/day10/Point.java
@@ -1,18 +1,48 @@
 package day10;
 
 public class Point {
-	
+
 	private int x;
 	private int y;
-	
+
 	public Point(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	public void setPoint(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 
+	@Override
+	public boolean equals(Object obj) {
+		Point p = (Point) obj;
+
+		if (x == p.x && y == p.y) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
+
+class Rect {
+	private int width;
+	private int height;
+
+	public Rect(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		Rect p = (Rect) obj;
+		if (width * height == p.width * p.height)
+			return true;
+		else
+			return false;
+	}
 }
--- java_exam/src/day10/PointMain.java
+++ java_exam/src/day10/PointMain.java
@@ -1,5 +1,7 @@
 package day10;
 
+import java.util.Scanner;
+
 public class PointMain {
 
 	public static void main(String[] args) {
@@ -8,6 +10,38 @@ public class PointMain {
 		Point p2 = new Point(1, 2);
 
 		System.out.println(p1 == p2);
+
+		Point a = new Point(2, 3);
+		Point b = new Point(2, 3);
+		Point c = new Point(3, 4);
+		Rect x = new Rect(2, 3);
+		Rect y = new Rect(3, 2);
+
+		if (a == b) {
+			System.out.println("a == b");
+		}
+
+		if (a.equals(b)) {
+			System.out.println("a is equal to b");
+		}
+
+		if (a.equals(c)) {
+			System.out.println("a is equal to c");
+		}
+
+		if (x.equals(y)) {
+			System.out.println("x is equal to y");
+		}
+
+		Scanner scan = new Scanner(System.in);
+		String name = "hong";
+		System.out.println("이름 : " + name);
+		System.out.print("이름 : ");
+
+		String name2 = scan.next();
+		System.out.println(name.equals(name2));
+
+		scan.close();
 
 	}
 
