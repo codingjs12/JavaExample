add68f6cf149a79f697b946f56c844739cb907d3
codingjs12 <202245068@itc.ac.kr>
2025-01-13 18:16:42 +0900

    250113 ASCII 코드

---
+++ java_exam/src/day12/ASCII.java
@@ -0,0 +1,15 @@
+package day12;
+
+public class ASCII {
+
+	public static void main(String[] args) {
+		
+		char c = 'a';
+		System.out.println(c + 10);
+		
+		char a = 'A';
+		System.out.println(a + 0);
+
+	}
+
+}
