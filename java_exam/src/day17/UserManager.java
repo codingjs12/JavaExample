434f749a11b0b6aa8f0b345a281dbec779b40a0d
codingjs12 <202245068@itc.ac.kr>
2025-01-20 11:28:50 +0900

    250120 오라클 자바 INSERT, SELECT

--- java_exam/src/day17/UserManager.java
+++ java_exam/src/day17/UserManager.java
@@ -1,5 +1,6 @@
 package day17;
 
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Scanner;
 
@@ -8,28 +9,44 @@ import day16.DBClass;
 public class UserManager {
 
 	public static void main(String[] args) {
-		
+
 		try {
 			Scanner scan = new Scanner(System.in);
 			DBClass db = new DBClass();
 			Statement stmt = db.stmt;
 			
-			System.out.print("아이디 : ");
-			String id = scan.next();
-			System.err.print("비밀번호 : ");
+			String id = "";
+			
+			while(true) {
+				System.out.print("아이디 : ");
+				String input = scan.next();
+				String select = "SELECT * FROM TBL_USER WHERE USERID = '" + input + "'";
+				
+				ResultSet rs = stmt.executeQuery(select);
+				
+				if(rs.next()) {
+					System.out.println("이미 존재하는 아이디 입니다.");
+				} else {
+					id = input;
+					break;
+				}
+				
+			}
+
+			System.out.print("비밀번호 : ");
 			String pwd = scan.next();
 			System.out.print("이름 : ");
 			String name = scan.next();
-			
-			String sql = "INSERT INTO TBL_USER (USERID, PASSWORD, USERNAME) VALUES('"
-					+ id + "' ,'" + pwd +"', '" + name +"')";
-			
+
+			String sql = "INSERT INTO TBL_USER (USERID, PASSWORD, USERNAME) VALUES('" + id + "' ,'" + pwd + "', '"
+					+ name + "')";
+
 			int result = stmt.executeUpdate(sql);
-			if(result > 0) {
+			if (result > 0) {
 				System.out.println("가입되었습니다!");
 			}
 			scan.close();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 	}
