0204b8a0b9c98e18a73b69eec4657358cb76d404
codingjs12 <202245068@itc.ac.kr>
2025-01-20 18:11:13 +0900

    250120 ArrayList, HashMap 복습

--- java_exam/src/day17/PersonMain.java
+++ java_exam/src/day17/PersonMain.java
@@ -5,20 +5,20 @@ import java.util.HashMap;
 public class PersonMain {
 
 	public static void main(String[] args) {
-		
+
 		Person hong = new Person("홍길동", 30);
 		System.out.println(hong);
-		
+
 		Student kim = new Student("김철수", 25, "20201234", 3);
-		
+
 		System.out.println(kim);
-		
+
 		System.out.println(kim.study("자바"));
-		
+
 		HashMap<String, Object> map = kim.getInfo();
-		
+
 		System.out.println(map.get("stuNo"));
 		System.out.println(map.get("grade"));
-		
+
 	}
 }
