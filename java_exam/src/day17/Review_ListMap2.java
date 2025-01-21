af820db822241dfadeb1cf2d5f1812a2e8609c15
codingjs12 <202245068@itc.ac.kr>
2025-01-21 10:06:24 +0900

    250121 return, break 차이

--- java_exam/src/day17/Review_ListMap2.java
+++ java_exam/src/day17/Review_ListMap2.java
@@ -31,11 +31,23 @@ public class Review_ListMap2 {
 		}
 
 		System.out.println(list);
-		
-		for(int i = 0; i < list.size(); i++) {
-			System.out.println(list.get(i));
+
+		System.out.print("수정할 사용자 입력 : ");
+		String name = scan.next();
+		boolean userFlg = false;
+
+		for (int i = 0; i < list.size(); i++) {
+			HashMap<String, Object> map = list.get(i);
+			if (map.get("이름").equals(name)) {
+				System.out.println(map);
+				userFlg = true;
+				break;
+			}
 		}
-		
+		if (userFlg == false) {
+			System.out.println("없는 사용자");
+		}
+
 		scan.close();
 	}
 }
---
+++ java_exam/src/day18/ListMap.java
@@ -0,0 +1,21 @@
+package day18;
+
+public class ListMap {
+	
+	public static void temp() {
+		
+		for(int i = 0; i < 100; i++) {
+			if(i == 50) {
+				return;
+			}
+			System.out.println(i);
+		}
+		System.out.println("test");
+	}
+
+	public static void main(String[] args) {
+		temp();
+		
+
+	}
+}
